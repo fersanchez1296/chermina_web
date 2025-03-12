3bd0eb9bb69753ecce1594d188a94fc9b906e18a
fersanchez1296 <fernandosanchezplacencia@gmail.com>
2025-03-12 12:00:43 -0600

    cambios en el componente presentation

--- src/components/Headers/PresentationHeader.js
+++ src/components/Headers/PresentationHeader.js
@@ -36,12 +36,38 @@ function PresentationHeader() {
         </div>
         <div className="rellax-text-container rellax-text">
           <h1 className="h1-seo" data-rellax-speed="-1">
-            Chermina
+            Now UI Kit
           </h1>
+          <div className="pro">PRO</div>
         </div>
         <h3 className="h3-description rellax-text" data-rellax-speed="-1">
-          Instituto de Pensiones del Estado de Jalisco
+          A beautiful premium Bootstrap 4 UI Kit.
         </h3>
+        <h6
+          className="category category-absolute rellax-text"
+          data-rellax-speed="-1"
+        >
+          Designed by{" "}
+          <a href="http://invisionapp.com/?ref=creativetim" target="_blank">
+            <img
+              alt="..."
+              className="invision-logo"
+              src={require("assets/img/invision-white-slim.png")}
+            ></img>
+          </a>
+          . Coded by{" "}
+          <a
+            href="https://www.creative-tim.com?ref=nuk-pro-react-presentation-header"
+            target="_blank"
+          >
+            <img
+              alt="..."
+              className="creative-tim-logo"
+              src={require("assets/img/creative-tim-white-slim2.png")}
+            ></img>
+          </a>
+          .
+        </h6>
       </div>
     </>
   );
